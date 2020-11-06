a16818331aa7c4861627920d9627354f29cba2d0
kento28 <atnorisiro@gmail.com>
2020-11-06 21:35:31 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -2,6 +2,6 @@ class LinebotController < ApplicationController
   require 'line/bot'
   def callback
     body = request.body.read
-
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
   end
 end
