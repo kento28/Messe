19d35e690e0c5287f8fbab7f64c957db1d008a02
kento28 <atnorisiro@gmail.com>
2020-11-21 19:11:44 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -2,8 +2,9 @@ class LinebotController < ApplicationController
   require 'line/bot'
   def callback
     body = request.body.read
-    unless 
-      
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' 
+      end
   end
   
 end
