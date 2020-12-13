ff6108b1069fde30064391f00adfec68ef23273c
kento28 <atnorisiro@gmail.com>
2020-12-13 23:58:11 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -20,7 +20,7 @@ class LinebotController < ApplicationController
           type: 'text',
           text: "#{seed1} × #{seed2} !!"
         }]
-        client.reply_message()
+        client.reply_message(message)
     end
   end
   private
